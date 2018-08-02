1b79a6a0796f1a8fb0eea3b65a39ea86be10de82
sunpengxx5 <1461692534@qq.com>
2018-08-02 09:02:52 +0800

    console-demo

--- consoledemo/target/console-demo/initDemo.sh
+++ consoledemo/target/console-demo/initDemo.sh
@@ -1,7 +1,4 @@
 dburl=${dburl//\//\\\/}
 dburl=${dburl//&/\\&}
-configPath=/usr/local/tomcat/webapps/ddmdemo/WEB-INF/classes/application.properties
-sed -i 's/\(jdbc.url=\).*/\1'$dburl'/' $configPath
-sed -i 's/\(jdbc.username=\).*/\1'$dbuser'/' $configPath
-sed -i 's/\(jdbc.password=\).*/\1'$dbpwd'/' $configPath
-sed -i 's/\(dbcp.maxActive=\).*/\1'$DbConnectionMaxActive'/' $configPath
+configPath=/usr/local/tomcat/webapps/console-demo/WEB-INF/classes/config/managerservices.properties
+sed -i 's/\(managerServicesUrl=\).*/\1'managerServicesUrl'/' $configPath
