101536112766bd7f2b4336753fb14131b008ce8d
sunpengxx5 <1461692534@qq.com>
2018-08-02 09:24:03 +0800

    demo

--- consoledemo/target/console-demo/initDemo.sh
+++ consoledemo/target/console-demo/initDemo.sh
@@ -1,4 +1,4 @@
-dburl=${dburl//\//\\\/}
-dburl=${dburl//&/\\&}
+managerServicesUrl=${managerServicesUrl//\//\\\/}
+managerServicesUrl=${managerServicesUrl//&/\\&}
 configPath=/usr/local/tomcat/webapps/console-demo/WEB-INF/classes/config/managerservices.properties
-sed -i 's/\(managerServicesUrl=\).*/\1'managerServicesUrl'/' $configPath
+sed -i 's/\(managerServicesUrl=\).*/\1'$managerServicesUrl'/' $configPath
